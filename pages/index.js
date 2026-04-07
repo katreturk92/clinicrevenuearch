e335a70263ce8b5dd695bc39d9ebd6dd9f633c92
katreturk92 <katreturk92@gmail.com>
2026-04-07 13:51:34 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
-
+console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL)
+console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
